0ef3dc89d481723cb836d4ee86411dd1f29bcb78
dinhqtai <dinhquoctai2003@gmail.com>
2023-08-27 14:22:21 +0700

    update

--- BE/src/routers/index.js
+++
@@ -1,6 +0,0 @@
-import express from "express"
-import routerProducts from "./products";
-
-const router = express.Router()
-router.use("/products", routerProducts)
-export default router;
--- BE/src/routers/products.js
+++
@@ -1,7 +0,0 @@
-import express from "express"
-import { getProducts, postProducts } from "../controllers/products";
-
-const routerProducts = express.Router()
-routerProducts.get("/", getProducts)
-routerProducts.post("/", postProducts)
-export default routerProducts;
